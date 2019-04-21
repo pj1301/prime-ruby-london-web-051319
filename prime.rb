81a3c226604a40ae88ea7275ddef31cf817ba146
James Hayward <james.hayward6@icloud.com>
2019-04-21 22:43:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,19 +4,19 @@ require "pry"
 def prime?(num)
 non_primes = []
 primes = []
-for i in 2..Float::INFINITY
-  if num <= 1
-    non_primes.push()
-  elsif num = 2
-      primes.push(num)
-    elsif num > 2 && num % i == 0
-      non_primes.push(num)
-    else
-      primes.push(num)
-    end
-    primes.include?(num) ? true : false
-  end
-end
+# for i in 2..Float::INFINITY
+#   if num <= 1
+#     non_primes.push()
+#   elsif num = 2
+#       primes.push(num)
+#     elsif num > 2 && num % i == 0
+#       non_primes.push(num)
+#     else
+#       primes.push(num)
+#     end
+#     primes.include?(num) ? true : false
+#   end
+# end
 
 #   if num % 2..Float::INFINITY == 0 || num < 0
 #     # binding.pry
