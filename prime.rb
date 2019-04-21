d550cf651c5c235d7837c6c2fab0b41e1f54b5f8
James Hayward <james.hayward6@icloud.com>
2019-04-21 21:30:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,13 @@ require "pry"
 
 # Add  code here!
 def prime?(num)
-  if num % 1..Float::INFINITY == 0 || num < 0
-    # binding.pry
-    false
-  elsif num % 1..Float::INFINITY != 0
-    true
-  end
-end
+#   if num % 1..Float::INFINITY == 0 || num < 0
+#     # binding.pry
+#     false
+#   elsif num % 1..Float::INFINITY != 0
+#     true
+#   end
+# end
 
 num > 0 && num % 1..Float::INFINITY == 0 ? false : true
+end
