87e2ecf97699e494fab71c75636116e617888caa
James Hayward <james.hayward6@icloud.com>
2019-04-21 21:19:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
   if num % 1..Float::INFINITY == 0
     return true
-  else num < 0 ||  num % 1..Float::INFINITY != 0
+  elsif num < 0 ||  num % 1..Float::INFINITY != 0
     return false
   # else
   #   return false
