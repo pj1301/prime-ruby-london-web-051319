e2e46c9cb01306835793809538fcb1651e9ecd73
James Hayward <james.hayward6@icloud.com>
2019-04-21 22:20:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,22 +5,22 @@ non_primes = []
 primes = []
 
 def prime?(num)
-#   if num % 1..Float::INFINITY == 0 || num < 0
-#     # binding.pry
-#     false
-#   elsif num % 1..Float::INFINITY != 0
-#     true
-#   end
-# end
+  if num % 2..Float::INFINITY == 0 || num < 0
+    # binding.pry
+    false
+  elsif num % 2..Float::INFINITY != 0
+    true
+  end
+end
 
 # num > 1 && num % 2..Float::INFINITY != 0 ? true : false
 # binding.pry
-  for i in 3..num - 1
-    if num % i == 0
-      non_primes.push(num)
-    else
-      primes.push(num)
-    end
-    primes.include?(num) ? true : false
-end
+#   for i in 3..num - 1
+#     if num % i == 0
+#       non_primes.push(num)
+#     else
+#       primes.push(num)
+#     end
+#     primes.include?(num) ? true : false
+# end
 end
